d3ecf799a5cae7fad4e2b514dd308a8797cc962c
Beréti Zsófia <beretizsofia@turr.hu>
2024-03-11 08:41:16 +0100

    userctrl login registration I.

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -14,20 +14,14 @@ app.controller('userCtrl', function($scope, $rootScope, $location, ngNotify){
                  
                  ngNotify.set('A megadott jelszó nem megfelelő erősségű!', 'error');
                 }else{
-                    let data = {
-                        email: $scope.user.email
-                    }
-                    axios.post($rootScope.serverUrl + '/db/emailcheck', data).then(res =>{
-                        if (res.data.length > 0){
-                            ngNotify.set('Ez az e-mail cím már regisztrálva van!', 'error');
-                           
-                        }else{
+                    
+                    
                             data = {
                                 name: $scope.user.name,
                                 email: $scope.user.email,
-                                passwd: CryptoJS.SHA1($scope.user.passwd).toString()
+                                passwd: $scope.user.passwd
                             }
-                            axios.post($rootScope.serverUrl + '/db/registration', data).then(res=>{
+                            axios.post($rootScope.serverUrl + '/users', data).then(res=>{
                                 if(res.data.affectedRows == 1){
                                     
                                     ngNotify.set('Sikeres regisztráció!', 'success');
@@ -38,29 +32,29 @@ app.controller('userCtrl', function($scope, $rootScope, $location, ngNotify){
                                     
                                 }
                             });
-                        }
-                    });
+                        
+                    
                 }
             }
         }
     }
 
     $scope.login = function(){
-        if ($scope.user.email == null || $scope.user.passwd == null){
+        if ($scope.user.name == null || $scope.user.passwd == null){
             ngNotify.set('Nem adtál meg minden adatot!!', 'error');
         }else{
             data = {
-                email: $scope.user.email,
-                passwd: CryptoJS.SHA1($scope.user.passwd).toString()
+                name: $scope.user.name,
+                passwd: $scope.user.passwd
             }
-            axios.post($rootScope.serverUrl + '/db/logincheck', data).then(res=>{
+            axios.post($rootScope.serverUrl + '/users', data).then(res=>{
                 if(res.data.token == ""){
                     ngNotify.set('Hibás belépési adatok!', 'error');
                 }else{
-                    sessionStorage.setItem('weatherApp', JSON.stringify(res.data.token));
+                    sessionStorage.setItem('cookBook', JSON.stringify(res.data.token));
                     ngNotify.set('Sikeres belépés', 'succes');
                     $rootScope.isLoggedIn = true;
-                    $location.path('/newdata');
+                  
                 }
             });
         }
