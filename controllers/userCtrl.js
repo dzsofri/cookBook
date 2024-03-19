8e0f684cec3a72ee5d3df1bd94480a68a71a45b6
Beréti Zsófia <beretizsofia@turr.hu>
2024-03-19 08:18:32 +0100

    működő commit

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -44,7 +44,7 @@
                 name: name.value,
                 passwd: passwd.value
             }
-            axios.post(`http://localhost:3000/logincheck`, data).then(res => {
+            axios.post(`http://localhost:3000/users`, data).then(res => {
                 if(res.data.length == 0){
                     showMessage("Hibás belépési adatok!");
                 }else
