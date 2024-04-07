ca41b54c6fe93ee6f8f27df8e92add9da228423f
dzsofri <beretizsofia@turr.hu>
2024-04-07 11:16:18 +0200

    navbar bug fix (1. rész)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,90 +1,64 @@
-var app = angular.module('cookBook', ['ngRoute', 'ngNotify', 'angular-uuid']);
+// app.js
 
+var app = angular.module('app', ['ngRoute', 'ngNotify', 'angular-uuid']);
 
-let loggedInMenu = document.querySelector('#loggedInMenu');
-let loggedOutMenu = document.querySelector('#loggedOutMenu');
-
-;
-
-
-
-function showMessage(msg){
-    let alertBox = document.querySelector('#alertBox');
-    alertBox.innerHTML = `<strong>HIBA!</strong> ${msg}`;
-    alertBox.classList.remove('d-none');
-}
-
-
-
-let loggedUser = sessionStorage.getItem('CookBook');
-
-
-app.run(function($rootScope, $location){
+app.run(function($rootScope, $location) {
     $rootScope.serverUrl = 'http://localhost:3000';
-    $rootScope.company = 'cookBook';
+    $rootScope.company = 'CookBook';
     $rootScope.year = new Date().getFullYear();
-   
-    if (loggedUser != null){
-        loggedOutMenu.classList.add('d-none');
-        loggedInMenu.classList.remove('d-none');
+
+    // Ellenőrizze, hogy a felhasználó be van-e jelentkezve
+    let loggedUser = localStorage.getItem('CookBookUser');
+    if (loggedUser != null) {
+        $rootScope.isLoggedIn = true;
         $location.path('/mainPage');
     } else {
-        loggedInMenu.classList.add('d-none');
-        loggedOutMenu.classList.remove('d-none');
+        $rootScope.isLoggedIn = false;
         $location.path('/login');
     }
-   
-   
 });
 
-
-app.config(function($routeProvider){
-
-   
+app.config(function($routeProvider) {
     $routeProvider
-    .when('/login', {
-        templateUrl: '/views/login.html',
-        controller: 'userCtrl'
-    })
-    .when('/register', {
-        templateUrl: '/views/register.html',
-        controller: 'userCtrl'
-    })
-    .when('/mainPage', {
-        templateUrl: '/views/fooldal.html',
-    })
-    .when('/breakfast', {
-        templateUrl: '/views/reggeli.html'
-    })
-    .when('/lunch', {
-        templateUrl: '/views/ebed.html'
-    })
-    .when('/appetizer', {
-        templateUrl: '/views/eloetel.html'
-    })
-    .when('/mainCourse', {
-        templateUrl: '/views/foetel.html'
-    })
-    .when('/sideDish', {
-        templateUrl: '/views/koret.html'
-    })
-    .when('/dessert', {
-        templateUrl: '/views/desszert.html'
-    })
-    .when('/dinner', {
-        templateUrl: '/views/vacsora.html'
-    })
-    .when('/myRecipes', {
-        templateUrl: '/views/sajatReceptek.html'
-    })
-     .when('/aboutUs', {
-        templateUrl: '/views/rolunk.html'
-    })
-    .otherwise(
-        {redirectTo: '/login'}
-    )
+        .when('/login', {
+            templateUrl: '/views/login.html',
+            controller: 'userCtrl'
+        })
+        .when('/register', {
+            templateUrl: '/views/register.html',
+            controller: 'userCtrl'
+        })
+        .when('/mainPage', {
+            templateUrl: '/views/fooldal.html'
+        })
+        .when('/breakfast', {
+            templateUrl: '/views/reggeli.html'
+        })
+        .when('/lunch', {
+            templateUrl: '/views/ebed.html'
+        })
+        .when('/appetizer', {
+            templateUrl: '/views/eloetel.html'
+        })
+        .when('/mainCourse', {
+            templateUrl: '/views/foetel.html'
+        })
+        .when('/sideDish', {
+            templateUrl: '/views/koret.html'
+        })
+        .when('/dessert', {
+            templateUrl: '/views/desszert.html'
+        })
+        .when('/dinner', {
+            templateUrl: '/views/vacsora.html'
+        })
+        .when('/myRecipes', {
+            templateUrl: '/views/sajatReceptek.html'
+        })
+        .when('/aboutUs', {
+            templateUrl: '/views/rolunk.html'
+        })
+        .otherwise({
+            redirectTo: '/login'
+        });
 });
-
-
-
-
